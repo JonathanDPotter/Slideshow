0f5ecdea5a7de1ef9e8aabcc4af21f7429f5e784
JonathanDPotter <jonathan.d.potter@outlook.com>
2021-05-06 16:35:18 -0400

    theme-chooser functional

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-import "./style.css";
+import "./style.scss";
+
+const setTheme = function (theme) {
+  const themed = [...document.querySelectorAll(".themed")];
+  themed.forEach((elem) => {
+    let shortList = [...elem.classList].splice(0, 2);
+    console.log(shortList);
+    elem.className = shortList[0];
+    elem.classList.add(shortList[1]);
+    elem.classList.add(theme);
+  });
+};
+
+const setup = function () {
+  // themes is an array of DOM elements with the theme class
+  const themes = [...document.querySelectorAll(".theme")];
+  
+  themes.forEach((theme) => {
+    theme.addEventListener("click", (event) => {
+      console.log(event.target)
+      event.preventDefault();
+      setTheme(event.target.classList[1]);
+    })
+  })
+};
+
+setup();
